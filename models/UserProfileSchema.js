42ac30231cf8b097f056cd68b2d90864a97c6a50
Samay Gupta <samay13g@icoud.com>
2022-10-21 10:59:14 -0700

    Finished creating the UserAccount & UserProfile schemas

--- models/UserProfileSchema.js
+++ models/UserProfileSchema.js
@@ -1,6 +1,52 @@
 const mongoose = require("mongoose");
 const uuid = require('uuid');
 
-const UserProfileSchema = mongoose.Schema({});
+const UserProfileSchema = mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    first_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email_id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (value.length < 5) throw new Error("Invalid email address.");
+      }
+    },
+    profile_image: {
+      data: Buffer,
+      contentType: String
+    },
+    public_account: {
+      type: Boolean,
+      default: true
+    },
+    library: {
+      type: Array,
+      default: []
+    },
+    published_works: {
+      type: Array,
+      default: []
+    },
+    unpublished_works: {
+      type: Array,
+      default: []
+    }
+  }, {collection : 'UserProfile'});
 
 module.exports = UserProfileSchema;
