176dd5c1f2e33c5efe421eac6e25606b2e573d3c
Shae Li <wo.1263202573@gmail.com>
2022-12-02 13:25:59 -0800

    Update IonicPenDB.test.js

--- models/IonicPenDB.test.js
+++ models/IonicPenDB.test.js
@@ -1,22 +1,110 @@
 const mongoose = require("mongoose");
-
-const config = require("../config");
-
+const bcrypt = require("bcrypt");
 const {
-  database: { host, name, username, password },
-} = config;
+  getAuthKeyFromCredentials,
+  getUserProfileFromAuthKey,
+  getUserProfileFromUsername,
+  createNewUserAccountAndProfile,
+  searchForKeyword,
+  getEBook,
+  getEBookmark,
+  setEBookmark,
+  getEBookChapterByID,
+  addBookToLibrary,
+  removeBookFromLibrary,
+  deleteBookFromDatabase,
+  getAllBooks,
+  publishExistingBook,
+  unpublishExistingBook,
+  createNewBook,
+  createNewChapter,
+} = require("./IonicPenDB");
 
-const DATABASE_NAME = name;
-const DATABASE_URL = `mongodb+srv://${username}:${password}@${host}/${DATABASE_NAME}?retryWrites=true&w=majority`;
+test("createNewUserAccountAndProfile", async () => {
+  await expect(createNewUserAccountAndProfile(
+    "tom123",
+    "tom",
+    "davis",
+    "tom_d@gmail.com",
+    await bcrypt.hash("123", 5)
+  )).rejects.toThrow('Username already exists!');
+  /*
+  .toBe(new Error("88a7db70-7222-11ed-bfcc-9977406adf0d"));
+  */
+});
+test("getAuthKeyFromCredentials", async () => {
+  await expect(getAuthKeyFromCredentials(
+    "tom123",
+    "123"
+  )).resolves.toBe("88a7db70-7222-11ed-bfcc-9977406adf0d");
+});
 
-let dbConnection = null;
+test("getUserProfileFromAuthKey", async () => {
+  await expect(getUserProfileFromAuthKey(
+    "88a7db70-7222-11ed-bfcc-9977406adf0d"
+  )).resolves.toHaveProperty("username", "tom123");
+});
 
-function getDatabaseConnection() {
-  if (!dbConnection) {
-    dbConnection = mongoose.createConnection(DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  }
-  return dbConnection;
-}
+test("getUserProfileFromUsername", async () => {
+  await expect(getUserProfileFromUsername("tom123"))
+  .resolves.toHaveProperty("username", "tom123");
+});
+/*
+test("searchForKeyword", () => {
+    await expect(searchForKeyword(
+      "88a7db70-7222-11ed-bfcc-9977406adf0d", ""
+    )).toBe("");
+});
+*/
+test("getEBook", async () => {
+  await expect(getEBook("382c14f0-6f60-11ed-9617-ff046d1a7ed9"))
+  .resolves.toHaveProperty("book_title", "Hidden Treasure");
+});
+test("getEBookmark", async () => {
+  await expect(getEBookmark(
+    "88a7db70-7222-11ed-bfcc-9977406adf0d", 
+    "382c14f0-6f60-11ed-9617-ff046d1a7ed9"
+  )).resolves.toHaveProperty("chapter_ind", 0);
+});
+/*
+test("", () => {
+    expect(setEBookmark("", "")).toBe("");
+});
+*/
+
+test("getEBookChapterByID", async () => {
+    await expect(getEBookChapterByID(
+      "44e104a0-7224-11ed-9755-4f043c7e6ed8"
+    )).resolves.toHaveProperty("chapter_name", "Chapter 3: The Treasure is Found");
+});
+/*
+test("", () => {
+    expect(addBookToLibrary("", "")).toBe("");
+});
+test("", () => {
+    expect(removeBookFromLibrary("", "")).toBe("");
+});
+test("", () => {
+    expect(deleteBookFromDatabase("", "")).toBe("");
+});
+test("", () => {
+    expect(getAllBooks("", "")).toBe("");
+});
+test("", () => {
+    expect(publishExistingBook("", "")).toBe("");
+});
+test("", () => {
+    expect(unpublishExistingBook("", "")).toBe("");
+});
+test("", () => {
+    expect(createNewBook("", "")).toBe("");
+});
+test("", () => {
+    expect(createNewChapter("", "")).toBe("");
+});
+*/
+afterAll((done) => {
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.disconnect();
+  done();
+})
